daaa6a999d484fff4698bb4b9e666827bd02945f
lsolomon12 <50463543+lsolomon12@users.noreply.github.com>
2020-11-17 11:09:28 -0500

    Delete pipeline_config.groovy

--- pipeline_config.groovy
+++
@@ -1,9 +0,0 @@
-libraries{
-  docker {
-    build_strategy = "dockerfile"
-    registry = "docker-registry.default.svc:5000"
-    cred = ""
-    repo_path_prefix = "localhost:5000"
-    remove_local_image = true
-  }
-}
